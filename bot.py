cd817fc88d983f30e6ec4a55467363aef3ff2604
Kisly93 <spsvk.kis.sov@gmail.com>
2023-09-24 16:33:30 +0200

    added loghandler

--- bot.py
+++ bot.py
@@ -8,15 +8,16 @@ import requests
 import telegram
 from dotenv import load_dotenv
 
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
 
-log_formatter = logging.Formatter('%(asctime)s | %(levelname)s | %(name)s | %(message)s')
+class TelegramLogHandler(logging.Handler):
+    def __init__(self, bot, chat_id):
+        super().__init__()
+        self.chat_id = chat_id
+        self.bot = bot
 
-log_file_handler = logging.FileHandler(f"{__name__}.log", mode='a')
-log_file_handler.setFormatter(log_formatter)
-
-logger.addHandler(log_file_handler)
+    def emit(self, record):
+        log_message = self.format(record)
+        self.bot.send_message(chat_id=self.chat_id, text=log_message)
 
 
 def send_telegram_notification(lesson_title, is_negative, lesson_url, chat_id, bot):
@@ -32,53 +33,47 @@ def send_telegram_notification(lesson_title, is_negative, lesson_url, chat_id, b
     bot.send_message(chat_id=chat_id, text=message)
 
 
-def log_to_chat_and_file(message, chat_id, bot):
-    bot.send_message(chat_id=chat_id, text=message)
-    logger.info(message)
-
-
 def main():
+    load_dotenv()
+    chat_id = os.getenv('CHAT_ID_TG')
+    telegram_token = os.getenv('TOKEN_TELEGRAM')
+    dwmn_token = os.getenv('DWMN_TOKEN')
+    bot = telegram.Bot(token=telegram_token)
+    bot.logger.addHandler(TelegramLogHandler(bot, chat_id))
+    bot.logger.warning('Бот запущен')
+    last_timestamp = None
     while True:
         try:
-            load_dotenv()
-            chat_id = os.getenv('CHAT_ID_TG')
-            telegram_token = os.getenv('TOKEN_TELEGRAM')
-            dwmn_token = os.getenv('DWMN_TOKEN')
-            bot = telegram.Bot(token=telegram_token)
-            log_message = "Бот запущен"
-            log_to_chat_and_file(log_message, chat_id, bot)
-            last_timestamp = None
-            while True:
-                url = 'https://dvmn.org/api/long_polling/'
-                headers = {
-                    "Authorization": dwmn_token
-                }
-                try:
-                    params = {'timestamp': last_timestamp}
-                    response = requests.get(url, headers=headers, params=params)
-                    response.raise_for_status()
-                    lesson_reviews = response.json()
+            url = 'https://dvmn.org/api/long_polling/'
+            headers = {
+                "Authorization": dwmn_token
+            }
+            try:
+                params = {'timestamp': last_timestamp}
+                response = requests.get(url, headers=headers, params=params)
+                response.raise_for_status()
+                lesson_reviews = response.json()
 
-                    if lesson_reviews['status'] == 'timeout':
-                        last_timestamp = lesson_reviews['timestamp_to_request']
-                    else:
-                        last_timestamp = lesson_reviews['last_attempt_timestamp']
-                        new_attempts = lesson_reviews['new_attempts']
-                        for new_attempt in new_attempts:
-                            lesson_title = new_attempt['lesson_title']
-                            is_negative = new_attempt['is_negative']
-                            lesson_url = new_attempt['lesson_url']
-                            send_telegram_notification(lesson_title, is_negative, lesson_url, chat_id, bot)
+                if lesson_reviews['status'] == 'timeout':
+                    last_timestamp = lesson_reviews['timestamp_to_request']
+                else:
+                    last_timestamp = lesson_reviews['last_attempt_timestamp']
+                    new_attempts = lesson_reviews['new_attempts']
+                    for new_attempt in new_attempts:
+                        lesson_title = new_attempt['lesson_title']
+                        is_negative = new_attempt['is_negative']
+                        lesson_url = new_attempt['lesson_url']
+                        send_telegram_notification(lesson_title, is_negative, lesson_url, chat_id, bot)
 
-                except requests.exceptions.Timeout:
-                    pass
-                except requests.exceptions.ConnectionError:
-                    error_message = "Интернет соединение отсутствует. Повторная попытка через 20 секунд..."
-                    log_to_chat_and_file(error_message, chat_id, bot)
-                    time.sleep(20)
+            except requests.exceptions.Timeout:
+                pass
+            except requests.exceptions.ConnectionError:
+                error_message = "Интернет соединение отсутствует. Повторная попытка через 20 секунд..."
+                bot.logger.warning(error_message)
+                time.sleep(20)
         except Exception as e:
             error_message = f"Бот упал с ошибкой: {str(e)}\n\n{traceback.format_exc()}"
-            log_to_chat_and_file(error_message, chat_id, bot)
+            bot.logger.warning(error_message)
 
 
 if __name__ == '__main__':
